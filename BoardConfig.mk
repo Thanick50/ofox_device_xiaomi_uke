8d4fbf510694de0829b06fdcb94c63c5f40e0381
thanick49 <sautthawit@gmail.com>
2026-07-26 17:33:40 +0700

    Add new libraries and configuration files for Xiaomi Uke device
    - Introduced multiple new binary libraries in the recovery/root/vendor/lib64 directory, including:
      - libjc_keymint_nxp.so
      - libjc_keymint_transport_nxp.so
      - libjsoncpp.so
      - libkeymasterdeviceutils.so
      - libkeymasterprovision.so
      - libkeymasterutils.so
      - libmdmdetect.so
      - libmemunreachable.so
      - libminkdescriptor.so
      - libminksocket_vendor.so
      - libmisight.so
      - libnetutils.so
      - libnicm.so
      - libnicm_dsi.so
      - libnicm_utils.so
      - libops.so
      - libqcbor.so
      - libqdi.so
      - libqdutils.so
      - libqisl.so
      - libqmi.so
      - libqmi_cci.so
      - libqmi_client_helper.so
      - libqmi_client_qmux.so
      - libqmi_common_so.so
      - libqmi_csi.so
      - libqmi_encdec.so
      - libqmiextservices.so
      - libqmiservices.so
      - libqrtr.so
      - libqservice.so
      - libqtikeymaster4.so
      - libqtikeymint.so
      - librecovery_updater.so
      - librecovery_updater_msm.so
      - librpmb.so
      - libsoc_helper.so
      - libspcom.so
      - libspl.so
      - libssd.so
      - libtaautoload.so
      - libtime_genoff.so
      - libtinyalsa.so
      - libvmmem.so
      - libxml.so
      - vendor.display.config@1.0.so
      - vendor.display.config@2.0.so
      - vendor.hardware.vibratorfeature.IVibratorExt-V1-ndk_platform.so
      - vendor.xiaomi.hardware.aidl.mtdservice-V1-ndk_platform.so
      - vendor.xiaomi.hw.touchfeature-V1-ndk_platform.so
    
    - Added ueventd.rc configuration file for device-specific settings.
    - Created system.prop file to define system properties for ADB and keymaster.
    - Added twrp_uke.mk for device configuration in TWRP recovery.
    - Implemented vendorsetup.sh for build environment setup specific to Xiaomi Uke.
    - Introduced xiaomi-uinput module for evdev to uinput touch bridge functionality.

---
+++ BoardConfig.mk
@@ -0,0 +1,187 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Copyright (C) 2025 The OrangeFox Recovery Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Building with minimal manifest
+ALLOW_MISSING_DEPENDENCIES := true
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := kryo
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := $(TARGET_ARCH_VARIANT)
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := $(TARGET_CPU_VARIANT)
+
+# Power
+ENABLE_CPUSETS := true
+ENABLE_SCHEDBOOST := true
+
+# Bootloader
+PRODUCT_PLATFORM := pineapple
+TARGET_BOOTLOADER_BOARD_NAME := uke
+TARGET_NO_BOOTLOADER := true
+TARGET_USES_UEFI := true
+
+# Platform
+TARGET_BOARD_PLATFORM := pineapple
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno735
+QCOM_BOARD_PLATFORMS += xiaomi_sm8635
+#BOARD_USES_QCOM_HARDWARE := true
+
+# Kernel
+BOARD_KERNEL_PAGESIZE         := 4096
+TARGET_KERNEL_ARCH            := arm64
+TARGET_KERNEL_HEADER_ARCH     := arm64
+BOARD_KERNEL_IMAGE_NAME       := Image
+BOARD_BOOT_HEADER_VERSION     := 4
+BOARD_MKBOOTIMG_ARGS          += --header_version $(BOARD_BOOT_HEADER_VERSION)
+BOARD_MKBOOTIMG_ARGS          += --pagesize $(BOARD_KERNEL_PAGESIZE)
+TARGET_PREBUILT_KERNEL        := $(DEVICE_PATH)/prebuilt/kernel
+BOARD_USES_GENERIC_KERNEL_IMAGE := true
+
+# Ramdisk use lz4
+BOARD_RAMDISK_USE_LZ4 := true
+
+# A/B
+BOARD_EXCLUDE_KERNEL_FROM_RECOVERY_IMAGE := true
+AB_OTA_UPDATER := true
+
+TW_V_AB_BOARD := true
+TW_INCLUDE_UPDATE_ENGINE := true
+TW_INCLUDE_UPDATE_ENGINE_SIDELOAD := true
+
+AB_OTA_PARTITIONS += \
+    boot \
+    init_boot \
+    vendor_boot \
+    dtbo \
+    vbmeta \
+    vbmeta_system \
+    odm \
+    product \
+    system \
+    system_ext \
+    system_dlkm \
+    vendor \
+    vendor_dlkm
+
+# Verified Boot
+BOARD_AVB_ENABLE := true
+
+# Partitions
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 104857600
+
+# Dynamic Partition
+BOARD_SUPER_PARTITION_SIZE := 8321499136
+BOARD_SUPER_PARTITION_GROUPS := qti_dynamic_partitions
+BOARD_QTI_DYNAMIC_PARTITIONS_SIZE := 8317304832
+BOARD_QTI_DYNAMIC_PARTITIONS_PARTITION_LIST := system system_ext product vendor vendor_dlkm system_dlkm odm
+
+BOARD_PARTITION_LIST := $(call to-upper, $(BOARD_QTI_DYNAMIC_PARTITIONS_PARTITION_LIST))
+$(foreach p, $(BOARD_PARTITION_LIST), $(eval BOARD_$(p)IMAGE_FILE_SYSTEM_TYPE := erofs))
+$(foreach p, $(BOARD_PARTITION_LIST), $(eval TARGET_COPY_OUT_$(p) := $(call to-lower, $(p))))
+
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := f2fs
+
+# File systems
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+BOARD_USES_VENDOR_DLKMIMAGE := true
+
+# Workaround for error copying vendor files to recovery ramdisk
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR := vendor
+
+# Recovery
+BOARD_HAS_LARGE_FILESYSTEM := true
+TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
+
+# Crypto
+FIXED_DECRYPT := true
+TW_INCLUDE_CRYPTO := $(FIXED_DECRYPT)
+TW_INCLUDE_CRYPTO_FBE := $(FIXED_DECRYPT)
+TW_INCLUDE_FBE_METADATA_DECRYPT := $(FIXED_DECRYPT)
+BOARD_USES_QCOM_FBE_DECRYPTION := $(FIXED_DECRYPT)
+TW_USE_FSCRYPT_POLICY := 2
+
+BOARD_USES_METADATA_PARTITION := true
+PLATFORM_VERSION := 99.87.36
+PLATFORM_SECURITY_PATCH := 2127-12-31
+PLATFORM_VERSION_LAST_STABLE := $(PLATFORM_VERSION)
+VENDOR_SECURITY_PATCH   := $(PLATFORM_SECURITY_PATCH)
+BOOT_SECURITY_PATCH     := $(PLATFORM_SECURITY_PATCH)
+
+# Board
+TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
+
+# Tool
+TW_INCLUDE_REPACKTOOLS := true
+TW_INCLUDE_RESETPROP := true
+TW_INCLUDE_LIBRESETPROP := true
+TW_INCLUDE_LPDUMP := true
+TW_INCLUDE_LPTOOLS := true
+
+# Debug
+TARGET_USES_LOGD := true
+TWRP_INCLUDE_LOGCAT := true
+
+# Fastbootd
+TW_INCLUDE_FASTBOOTD := true
+
+# Other TWRP Configurations
+TW_THEME := portrait_hdpi
+TW_ROTATION := 270
+TW_FRAMERATE := 120
+RECOVERY_SDCARD_ON_DATA := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_INCLUDE_NTFS_3G := true
+TW_NO_EXFAT_FUSE := false
+TW_USE_TOOLBOX := true
+TARGET_USES_MKE2FS := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
+TW_MAX_BRIGHTNESS := 2047
+TW_EXTRA_LANGUAGES := true
+TW_DEFAULT_LANGUAGE := en
+TW_DEFAULT_BRIGHTNESS := 200
+TW_NO_SCREEN_BLANK := true
+TW_EXCLUDE_APEX := true
+TW_HAS_EDL_MODE := true
+TW_NO_HAPTICS := true
+TW_USE_SERIALNO_PROPERTY_FOR_DEVICE_ID := true
+TW_LOAD_VENDOR_MODULES := "qti_battery_charger.ko si_haptic.ko aw8697-haptic.ko spi-msm-geni.ko i2c-msm-geni.ko panel_event_notifier.ko miev.ko msm_drm.ko metis.ko xiaomi_touch.ko nt36532_touch.ko"
+TW_LOAD_VENDOR_DLKM_MODULES := true
+TW_LOAD_VENDOR_MODULES_EXCLUDE_GKI := true
+TW_CUSTOM_CPU_TEMP_PATH := "/sys/class/thermal/thermal_zone48/temp"
+TW_BATTERY_SYSFS_WAIT_SECONDS := 6
+TW_OXYGENOS_WORKAROUND := true
+
+
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libdmabufheap
+
+TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libdmabufheap.so
---
+++ device.mk
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/xiaomi/uke
+
+# Base product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+
+# Virtual A/B OTA configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression_with_xor.mk)
+
+# Emulated storage support
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+# Configure twrp common.mk
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Device-specific OrangeFox configuration
+$(call inherit-product, $(DEVICE_PATH)/fox_uke.mk)
+
+# API
+PRODUCT_SHIPPING_API_LEVEL := 34
+PRODUCT_TARGET_VNDK_VERSION := 34
+BOARD_SHIPPING_API_LEVEL := 34
+SHIPPING_API_LEVEL := 34
+
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
+
+# Touchscreen uinput wrapper daemon
+PRODUCT_PACKAGES += \
+    xiaomi-uinput
+
